3773773570f3e94a9173f0b28ba9d7c22e172645
Ernest <appiahernest541@gmail.com>
2023-05-22 09:07:08 +0000

    first commit

---
+++ java.js
@@ -0,0 +1,11 @@
+console.log(8*3)
+console.log(24/2)
+console.log(84%7)
+console.log(10-20)
+
+var name = "Ernest"
+var contact = "0559382241"
+ console.log(name+contact)
+ console.log(name-contact)
+
+ 
